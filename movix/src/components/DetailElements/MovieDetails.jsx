4b65177ce022732617c4b81774778cf4901aa007
brunoascenc <brunoascenc1@outlook.com>
2020-11-18 21:19:30 -0300

    displaying movie details from api

---
+++ movix/src/components/DetailElements/MovieDetails.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+const IMAGE_URL = process.env.REACT_APP_IMAGE_URL;
+
+const MovieDetails = ({movieDetail}) => {
+    return (
+        <div id="movie-detail">
+          <div className="poster-movie">
+            <img src={IMAGE_URL + movieDetail.poster_path} />
+            <div className="bgimg"></div>
+          </div>
+          <div className="details-container">
+            <div className="movie-title">
+              <span></span>
+              <h1>{movieDetail.title}</h1>
+            </div>
+            <div className="overview">
+              <div className="title-section">
+                <span></span>
+                <h1>Movie overview</h1>
+              </div>
+              <article>
+                <p>{movieDetail.overview}</p>
+              </article>
+            </div>
+
+            <div className="detail-info">
+              <div className="title-section">
+                <span></span>
+                <h1>Movie Details</h1>
+              </div>
+              <ul>
+                <li>
+                  <span className="contrast">Title:</span> {movieDetail.title}
+                </li>
+                <li>
+                  <span className="contrast">Release Date:</span>
+                  {movieDetail.release_date}
+                </li>
+
+                <li>
+                  <span className="contrast">Duration:</span>{" "}
+                  {movieDetail.runtime} min
+                </li>
+                <li>
+                  <span className="contrast">Rating</span>{" "}
+                  {movieDetail.vote_average}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default MovieDetails
--- movix/src/components/Details.jsx
+++ movix/src/components/Details.jsx
@@ -1,35 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import MovieDetails from "./DetailElements/MovieDetails";
 import "../App.css";
 
 const API_KEY = process.env.REACT_APP_MOVIEDB_KEY;
 
 const Details = (props) => {
-  const [movieDetail, setMovieDetail] = useState([])
+  const [movieDetail, setMovieDetail] = useState([]);
+  
   const getDetails = () => {
-    axios.get(
-      `https://api.themoviedb.org/3/movie/${props.match.params.id}?api_key=${API_KEY}`
-    )
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${props.match.params.id}?api_key=${API_KEY}`
+      )
+      .then((res) => {
+        const response = res.data;
+        setMovieDetail(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  useEffect(() =>{
-    getDetails()
-  },[])
+  useEffect(() => {
+    getDetails();
+  }, []);
 
   return (
-    <div>
-      <button>aaaaaaaaaaaaaaa</button>
+    <>
       <div id="movie-banner">
         <header className="details-header">
-          <a href="index.html">
+          <Link to="/">
             <h1>Movix</h1>
-          </a>
+          </Link>
           <div className="back-home">
             <a href="/#">Back to home</a>
           </div>
@@ -37,11 +41,11 @@ const Details = (props) => {
         <div className="movie-trailer"></div>
       </div>
 
-      <div className="about-movie">
-        <div id="movie-detail"></div>
+      <div className="about-movie container">
+        <MovieDetails movieDetail={movieDetail} />
       </div>
-    </div>
+    </>
   );
 };
 
-export default Details
+export default Details;
