0008957fb278c515289759dc811d52ec6de85705
brunoascenc <brunoascenc1@outlook.com>
2021-01-22 12:57:54 -0300

    now playing banner

--- movix/src/components/LandingPage/LandingPage.jsx
+++ movix/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,6 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 import { DataContext } from "../../data/DataProvider";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Autoplay } from "swiper";
 import GenreList from "../Genres/GenreList";
 import { Link } from "react-router-dom";
@@ -15,16 +15,14 @@ const API_KEY = process.env.REACT_APP_MOVIEDB_KEY;
 const IMAGE_URL = process.env.REACT_APP_IMAGE_URL;
 
 const LandingPage = () => {
-    const value = useContext(DataContext);
-    const [nowPlaying] = value.nowPlaying
-    const [genreName] = GenreList();
+  const value = useContext(DataContext);
+  const [nowPlaying] = value.nowPlaying;
+  const [genreName] = GenreList();
 
-
-
-    return (
-        <section className="landing-page">
-              <Swiper
-        spaceBetween={65}
+  return (
+    <section className="landing-page">
+      <Swiper
+        // spaceBetween={65}
         slidesPerView={1}
         // navigation
         autoplay={{ delay: 3000, disableOnInteraction: false }}
@@ -36,26 +34,36 @@ const LandingPage = () => {
                 <Link to={`/details/${movie.id}`}>
                   <img
                     className="movie-poster"
-                    src={'https://image.tmdb.org/t/p/original' + movie.backdrop_path}
+                    src={
+                      "https://image.tmdb.org/t/p/original" +
+                      movie.backdrop_path
+                    }
                     data-movie-id={movie.id}
                     alt={movie.title}
                   />
+                  <div className="overlay"></div>
 
                   <div className="landing-txt">
-                      <h1>Now Playing</h1>
-                      <p>{movie.title}</p>
-                      <span className="movie-genres">{genreName(movie)}</span>
-                      <p>{movie.vote_average}</p>
+                      <h1>Now Playing </h1>
+                    <span className="movie-title">{movie.title}</span>
+                    <div>
+                    <p className="movie-genres">{genreName(movie)}</p> | 
+                    <div className="movie-rating">
+                    <i className="far fa-star"></i>
+                    <p>{movie.vote_average}</p>
+                  
+                  
+                   </div>
+                    </div>
+                    
                   </div>
                 </Link>
               </SwiperSlide>
             );
           })}
       </Swiper>
-            
-            
-        </section>
-    )
-}
+    </section>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
