e1b8ee46f599ec66861de0442e14e974388ff788
brunoascenc <brunoascenc1@outlook.com>
2021-06-24 13:21:55 -0300

    movies card

--- movix/src/components/PopularMovies/PopularMovies.jsx
+++ movix/src/components/PopularMovies/PopularMovies.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPopularMovies } from '../../redux/popular-movies/getPopularMovies';
-import { Link } from 'react-router-dom';
 import useGenres from '../../hooks/useGenres';
 import usePagination from '../../hooks/usePagination';
 import FullPageLoader from '../FullPageLoader/FullPageLoader';
-
-const IMAGE_URL = process.env.REACT_APP_IMAGE_URL;
+import MoviesCard from '../MoviesCard/MoviesCard';
 
 const PopularMovies = () => {
   const [genreName] = useGenres();
@@ -31,29 +29,7 @@ const PopularMovies = () => {
         {loading ? (
           <FullPageLoader />
         ) : (
-          popularMovie &&
-          popularMovie.map((movie) => {
-            return (
-              <div key={movie.id} className="movie-item">
-                <Link to={`/details/${movie.id}`}>
-                  <div className="img-container">
-                    <img
-                      className="movie-poster"
-                      src={IMAGE_URL + movie.poster_path}
-                      alt={IMAGE_URL}
-                      data-movie-id={movie.id}
-                    />
-                  </div>
-                </Link>
-                <span className="movie-title">{movie.title}</span>
-                <span className="movie-genres">{genreName(movie)}</span>
-                <div className="movie-rating">
-                  <i className="far fa-star"></i>
-                  <p>{movie.vote_average}</p>
-                </div>
-              </div>
-            );
-          })
+          <MoviesCard movies={popularMovie} genreName={genreName} />
         )}
       </div>
       <div className="pagination-btn">
