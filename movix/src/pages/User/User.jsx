3a80fa440a64d145c6223def1ef458c6497eac0b
brunoascenc <brunoascenc1@outlook.com>
2021-06-30 12:22:18 -0300

    user page

---
+++ movix/src/pages/User/User.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { fetchUserDetails } from '../../redux/user-details/userDetailsAction';
+import { selectUserDetails } from '../../redux/user-details/userDetailsSelector';
+import { fetchFavoriteMovies } from '../../redux/user-favorites/userFavoritesActions';
+import { selectSessionId } from '../../redux/user-session/userSessionSelector';
+
+const User = ({
+  userId,
+  fetchUserDetails,
+  userDetails,
+  fetchFavoriteMovies,
+}) => {
+  const username = userDetails.details.username;
+  useEffect(() => {
+    fetchUserDetails(userId.sessionId);
+    fetchFavoriteMovies(userId.sessionId);
+  }, [userId.sessionId, fetchUserDetails]);
+
+  return (
+    <div>
+      <h1>Olá {username}</h1>
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  userId: selectSessionId,
+  userDetails: selectUserDetails,
+});
+
+const mapDipatchToProps = (dispatch) => ({
+  fetchUserDetails: (id) => dispatch(fetchUserDetails(id)),
+  fetchFavoriteMovies: (id) => dispatch(fetchFavoriteMovies(id)),
+});
+
+export default connect(mapStateToProps, mapDipatchToProps)(User);
