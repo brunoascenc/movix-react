caa0af4daa172aea7f97a2684fa5ac5ccdf58534
brunoascenc <brunoascenc1@outlook.com>
2021-07-01 14:00:47 -0300

    login page style

--- movix/src/pages/Login/Login.jsx
+++ movix/src/pages/Login/Login.jsx
@@ -4,10 +4,12 @@ import { fetchTokenAuth } from '../../redux/user-token/userTokenActions';
 
 const Login = ({ fetchTokenAuth }) => {
   return (
-    <div>
-      <h1>Movix</h1>
-      <p>Login to save your favorite movies</p>
-      <button onClick={fetchTokenAuth}>Login</button>
+    <div className="login-page">
+      <div className="content">
+        <h1>Movix</h1>
+        <p>Login to save your favorite movies</p>
+        <button onClick={fetchTokenAuth}>Login</button>
+      </div>
     </div>
   );
 };
