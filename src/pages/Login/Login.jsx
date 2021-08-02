8d2b21ae24b11375b1f2d8becb8b361fabfbef24
brunoascenc <brunoascenc1@outlook.com>
2021-08-01 23:56:46 -0300

    login container

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchTokenAuth } from '../../redux/user-token/userTokenActions';
+import { LoginContainer } from './LoginStyles';
 
 const Login = ({ fetchTokenAuth }) => {
   return (
-    <div className="login-page">
-      <div className="content">
-        <h1>Movix</h1>
-        <p>Login to save your favorite movies</p>
-        <button onClick={fetchTokenAuth}>Login</button>
-      </div>
-    </div>
+    <LoginContainer>
+      <h1>Movix</h1>
+      <p>Login to save your favorite movies</p>
+      <button onClick={fetchTokenAuth}>Login</button>
+    </LoginContainer>
   );
 };
 
