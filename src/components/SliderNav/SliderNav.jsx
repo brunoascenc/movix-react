e4462ca602fc1b15a53da95310f138a8e7bb8343
brunoascenc <brunoascenc1@outlook.com>
2021-08-04 14:17:46 -0300

    slider nav buttons

---
+++ src/components/SliderNav/SliderNav.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import styled from 'styled-components';
+
+const LeftArrow = styled(MdKeyboardArrowLeft)`
+  cursor: pointer;
+  font-size: 18px;
+  color: #e0d2d1;
+  position: absolute;
+  top: 50%;
+  left: 15px;
+`;
+
+const RightArrow = styled(MdKeyboardArrowRight)`
+  cursor: pointer;
+  font-size: 18px;
+  color: #e0d2d1;
+  position: absolute;
+  top: 50%;
+  right: 21px;
+`;
+
+export const SliderNav = ({ prev, next }) => {
+  return (
+    <>
+      <LeftArrow className={prev} />
+      <RightArrow className={next} />
+    </>
+  );
+};
