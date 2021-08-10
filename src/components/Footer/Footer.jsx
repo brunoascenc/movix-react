8e8a893a6b8ecedab0276323e7e2c1c640bcbf81
brunoascenc <brunoascenc1@outlook.com>
2021-08-10 12:58:48 -0300

    footer background color space-evenly and icons style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,44 @@
-import React from "react";
-import Logo from "../../images/tmdblogo.svg";
-import {FooterContainer,TMDBLogo, FooterContent, Text, TMDBSocials, SocialsLinks} from './FooterStyles';
-import "../../App.css";
+import React from 'react';
+import Logo from '../../images/tmdblogo.svg';
+import {
+  FooterContainer,
+  TMDBLogo,
+  FooterContent,
+  Text,
+  TMDBSocials,
+  SocialsLinks,
+} from './FooterStyles';
+import '../../App.css';
 
 const Footer = () => {
   return (
     <FooterContainer className="container">
       <FooterContent>
-        <Text><span>Movix</span> © 2021</Text>
-        <Text>Created by <a href="https://brunoascencao.web.app/">Bruno Ascenção</a></Text>
+        <h3>About Movix</h3>
+        <Text>
+          Search and discover new movies and releases with movix! The data were
+          taken from the TMDB api.
+        </Text>
+        <Text copy>
+          <span>Movix</span> © 2021
+        </Text>
+        <Text copy>
+          Created by <a href="https://brunoascencao.web.app/">Bruno Ascenção</a>
+        </Text>
       </FooterContent>
       <TMDBSocials>
         <TMDBLogo src={Logo} alt="logo" />
         <SocialsLinks>
-        <a href="https://twitter.com/themoviedb">
-          <i className="fab fa-twitter"></i>
-        </a>
+          <a href="https://twitter.com/themoviedb">
+            <i className="fab fa-twitter"></i>
+          </a>
 
-        <a href="https://www.facebook.com/themoviedb">
-          <i className="fab fa-facebook-f"></i>
-        </a>
-        <a href="https://www.themoviedb.org/documentation/api">
-          <i className="fas fa-link"></i>
-        </a>
+          <a href="https://www.facebook.com/themoviedb">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.themoviedb.org/documentation/api">
+            <i className="fas fa-link"></i>
+          </a>
         </SocialsLinks>
       </TMDBSocials>
     </FooterContainer>
--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -5,16 +5,21 @@ export const FooterContainer = styled.footer`
   height: 250px;
   background: #0d0d12;
   background: rgb(11, 9, 13);
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
-  /* margin-top: 100px; */
+  background: rgb(11, 9, 13);
 `;
 
 export const TMDBLogo = styled.img`
-  width: 150px;
+  width: 145px;
 `;
 
 export const FooterContent = styled.div`
+  h3 {
+    font-size: 14px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+  }
   a {
     color: #a9a9b8;
     border-bottom: dotted 1px #5d5d66;
@@ -22,9 +27,12 @@ export const FooterContent = styled.div`
 `;
 
 export const Text = styled.p`
-  font-size: 14px;
+  font-size: ${(props) => (props.copy ? '12px' : '14px')};
   margin-top: 7px;
-  color: #91919c;
+  margin-bottom: ${(props) => (props.copy ? '-7px' : '0')};
+  color: ${(props) => (props.copy ? '#8f8f96' : '#91919c')};
+  width: 300px;
+  line-height: 21px;
 `;
 
 export const TMDBSocials = styled.div`
@@ -34,8 +42,24 @@ export const TMDBSocials = styled.div`
 `;
 
 export const SocialsLinks = styled.div`
+  margin-top: 2px;
+  display: flex;
   a {
     color: #91919c;
     margin: 5px;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    padding: 15px;
+    border-radius: 50%;
+    align-items: center;
+    border: solid 1px rgb(195, 191, 199, 0.2);
+    transition: 0.2s ease;
+    font-size: 13px;
+
+    &:hover {
+      border: solid 1px rgb(195, 191, 199, 0.4);
+    }
   }
 `;
