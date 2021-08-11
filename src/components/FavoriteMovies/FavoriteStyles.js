be46129b90cd2358fef4f13249b9922ff5da91ea
brunoascenc <brunoascenc1@outlook.com>
2021-08-11 16:04:29 -0300

    user list style

--- src/components/FavoriteMovies/FavoriteStyles.js
+++ src/components/FavoriteMovies/FavoriteStyles.js
@@ -2,19 +2,61 @@ import styled from 'styled-components';
 
 export const FavoriteSection = styled.div`
   margin-top: 150px;
+  display: flex;
+  flex-direction: column;
+  span {
+    font-size: 11px;
+    align-self: flex-end;
+    color: #8f8f96;
+    margin-right: 12px;
+    margin-top: 10px;
+  }
+`;
+
+export const Button = styled.button`
+  color: #5c5a5e;
+  font-size: 11px;
+  height: 20px;
+  margin-bottom: 15px;
+  background: none;
+  border: none;
+  align-self: center;
+  cursor: pointer;
+  transition: 0.1s ease;
+  display: flex;
+  align-items: center;
+  margin-top: -10px;
+
+  &:hover {
+    color: #e9f0eb;
+
+    .delete-icon {
+      color: #e9f0eb;
+    }
+  }
+
+  .delete-icon {
+    font-size: 13px;
+    color: #5c5a5e;
+    margin-left: 2px;
+  }
 `;
 
 export const FavoriteContainer = styled.div`
   display: flex;
   overflow-x: scroll;
 
+  .poster-card {
+    margin-bottom: 15px;
+  }
+
   img {
     margin-right: 15px;
-    margin-bottom: 20px;
+    /* margin-bottom: 20px; */
   }
 
   &::-webkit-scrollbar {
-    height: 6px;
+    height: 4px;
     border-radius: 12px;
   }
 
@@ -25,3 +67,30 @@ export const FavoriteContainer = styled.div`
 `;
 
 export const ListCard = styled.div``;
+
+export const FavoriteHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Filter = styled.div`
+  display: flex;
+  align-items: center;
+
+  h4 {
+    font-weight: 100;
+    margin-right: 5px;
+    color: #e9f0eb;
+  }
+`;
+
+export const Select = styled.select`
+  background: rgb(15, 12, 18);
+  border-radius: 16px;
+  padding: 6px 9px;
+  border: solid 1px rgb(51, 43, 54, 0.2);
+  color: #efe6e3;
+  transition: 0.1s ease;
+  font-size: 12px;
+`;
--- src/components/Watchlist/WatchlistStyles.js
+++ src/components/Watchlist/WatchlistStyles.js
@@ -1,15 +1,59 @@
 import styled from 'styled-components';
+import { CustomButton } from '../CustomButton/CustomButton';
 
 export const WatchlistSection = styled.div`
   margin-top: 80px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 100px;
+  span {
+    font-size: 11px;
+    align-self: flex-end;
+    color: #8f8f96;
+    margin-right: 12px;
+    margin-top: 10px;
+  }
+`;
+
+export const Button = styled.button`
+  color: #5c5a5e;
+  font-size: 11px;
+  height: 20px;
+  margin-bottom: 15px;
+  background: none;
+  border: none;
+  align-self: center;
+  cursor: pointer;
+  transition: 0.1s ease;
+  display: flex;
+  align-items: center;
+  margin-top: -10px;
+
+  &:hover {
+    color: #e9f0eb;
+
+    .delete-icon {
+      color: #e9f0eb;
+    }
+  }
+
+  .delete-icon {
+    font-size: 13px;
+    color: #5c5a5e;
+    margin-left: 2px;
+  }
 `;
 
 export const WatchlistContainer = styled.div`
   display: flex;
   overflow-x: scroll;
-  margin-bottom: 100px;
+
+  .poster-card {
+    margin-bottom: 15px;
+  }
+
   &::-webkit-scrollbar {
-    height: 6px;
+    height: 4px;
     border-radius: 12px;
   }
 
@@ -19,7 +63,7 @@ export const WatchlistContainer = styled.div`
   }
   img {
     margin-right: 15px;
-    margin-bottom: 20px;
+    /* margin-bottom: 20px; */
   }
 `;
 
@@ -35,4 +79,38 @@ export const ListCard = styled.div``;
 
 export const SectionTitle = styled.h2`
   margin-bottom: 15px;
+`;
+
+export const WatchlistHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Filter = styled.div`
+  display: flex;
+  align-items: center;
+
+  h4 {
+    font-weight: 100;
+    margin-right: 5px;
+    color: #e9f0eb;
+  }
+
+  span {
+    font-size: 11px;
+    color: #8f8f96;
+    margin-right: 12px;
+    margin-top: 2px;
+  }
+`;
+
+export const Select = styled.select`
+  background: rgb(15, 12, 18);
+  border-radius: 16px;
+  padding: 6px 9px;
+  border: solid 1px rgb(51, 43, 54, 0.2);
+  color: #efe6e3;
+  transition: 0.1s ease;
+  font-size: 12px;
 `;
