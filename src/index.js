c838550eca26c7d72ee8d27f7f053e947747fd38
brunoascenc <brunoascenc1@outlook.com>
2021-08-21 13:39:02 -0300

    changing hashrouter to browserrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
