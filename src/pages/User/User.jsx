57d1ade6aefb54e5bfd53f85963778444d42baa7
brunoascenc <brunoascenc1@outlook.com>
2021-08-24 10:27:38 -0300

    using hooks to handle redux

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   fetchUserDetails,
   fetchUserRequest,
 } from '../../redux/user-details/userDetailsAction';
-import { selectUserDetails } from '../../redux/user-details/userDetailsSelector';
-import { selectSessionId } from '../../redux/user-session/userSessionSelector';
 import FullPageLoader from '../../components/FullPageLoader/FullPageLoader';
 import { UserIconBg, UserHeader, UserInfo } from './UserStyles';
 import { signOutSuccess } from '../../redux/user-session/userSessionActions';
@@ -18,50 +14,46 @@ import UserLists from '../../components/UserLists/UserLists';
 import { removeFromWatchlist } from '../../redux/user-watchlist/watchlistUtils';
 import { removeFromFavorite } from '../../redux/user-favorites/favoritesUtils';
 
-const User = ({
-  userId,
-  userDetails,
-  signOutSuccess,
-  fetchWatchlistMovies,
-  fetchFavoriteMovies,
-}) => {
+const User = () => {
   const loading = useSelector((state) => state.user.loading);
   const dispatch = useDispatch();
-  const user = userDetails.details;
-  const username = user.username;
-  const watchlist = useSelector((state) => state.userWatchlist.results);
-  const favorites = useSelector((state) => state.userFavorites.results);
-  const favoriteMovies = favorites.results;
-  const moviesWatchlist = watchlist.results;
+  const watchlistTotal = useSelector((state) => state.userWatchlist.results);
+  const favoritesTotal = useSelector((state) => state.userFavorites.results);
+  const user = useSelector((state) => state.user.details);
+  const userId = useSelector((state) => state.sessionId);
+  const favoriteMovies = favoritesTotal.results;
+  const moviesWatchlist = watchlistTotal.results;
 
   useEffect(() => {
     dispatch(fetchUserRequest());
     setTimeout(() => {
       dispatch(fetchUserDetails(userId.sessionId));
-      fetchWatchlistMovies(userId.sessionId);
-      fetchFavoriteMovies(userId.sessionId);
+      dispatch(fetchWatchlistMovies(userId.sessionId));
+      dispatch(fetchFavoriteMovies(userId.sessionId));
     }, 700);
-  }, [userId.sessionId, dispatch, fetchWatchlistMovies, fetchFavoriteMovies]);
+  }, [userId.sessionId, dispatch]);
 
   const handleSignOut = () => {
-    signOutSuccess();
+    dispatch(signOutSuccess());
   };
 
   return (
     <div className="user-page">
       <UserHeader>
         <UserIconBg />
-        <span className="user-icon">{username && username.charAt(0)}</span>
+        <span className="user-icon">
+          {user.username && user.username.charAt(0)}
+        </span>
         <UserInfo>
           <p>{user.name === '' ? user.username : user.name}</p>
           <div className="info-details">
             <div className="list-info">
-              <span>{favorites.total_results}</span>
+              <span>{favoritesTotal.total_results}</span>
               <p>Favorite Movies</p>
             </div>
             <span className="separator"> </span>
             <div className="list-info">
-              <span>{watchlist.total_results}</span>
+              <span>{watchlistTotal.total_results}</span>
               <p>Watchlist Total</p>
             </div>
             <span className="separator"> </span>
@@ -95,16 +87,4 @@ const User = ({
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  userId: selectSessionId,
-  userDetails: selectUserDetails,
-});
-
-const mapDipatchToProps = (dispatch) => ({
-  fetchUserDetails: (id) => dispatch(fetchUserDetails(id)),
-  signOutSuccess: () => dispatch(signOutSuccess()),
-  fetchWatchlistMovies: (id) => dispatch(fetchWatchlistMovies(id)),
-  fetchFavoriteMovies: (id) => dispatch(fetchFavoriteMovies(id)),
-});
-
-export default connect(mapStateToProps, mapDipatchToProps)(User);
+export default User;
