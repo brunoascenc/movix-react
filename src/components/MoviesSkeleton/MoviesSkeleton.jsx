3a3f575b769659c995a488a671c1bade21765fa1
brunoascenc <brunoascenc1@outlook.com>
2021-09-09 10:55:28 -0300

    gradient animation colors

--- src/components/MoviesSkeleton/MoviesSkeleton.jsx
+++ src/components/MoviesSkeleton/MoviesSkeleton.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
 const Gradient = keyframes`
-    0%{
-        background-position:0% 9%;
+  0%{
+    background-position:0% 50%
+  }
+  50%{
+    background-position:100% 50%
+  }
+  100%{
+    background-position:0% 50%
     }
-    50%{
-        background-position:100% 92%;
-    }
-    100%{
-        background-position:0% 9%;
-        }
 `;
 
 const Skeleton = styled.div`
@@ -18,8 +18,8 @@ const Skeleton = styled.div`
   width: ${(props) => (props.resultsCard ? '230px' : '175px')};
   height: ${(props) => (props.resultsCard ? '350px' : '260px')};
   background: rgb(44, 44, 51, 0.2);
-  background: linear-gradient(100deg, #0b090d, #141417);
-  animation: ${Gradient} 2s ease infinite;
+  background: linear-gradient(268deg, #18161a, #0b090d);
+  animation: ${Gradient} 1.5s ease infinite;
   border-radius: 12px;
   border: solid 1px rgb(44, 44, 51, 0.2);
   background-size: 400% 400%;
